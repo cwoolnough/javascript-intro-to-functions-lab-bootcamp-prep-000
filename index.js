40ad5e9703e92a785826b270f0282bf5f4937976
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-08-23 10:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase() {
-    return "I can't hear you"; 
-  } else (string=== string.uppercase() {
-    return "YES INDEED";
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"; 
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else {
+    return "I love you, too.";
   }
 }
